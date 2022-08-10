ef653b8f5dd898a5400633f351d9f0b17f596d50
Pinhasov_Margarita <45937245+rita10514@users.noreply.github.com>
2022-08-10 12:29:46 +0300

    Delete FS1Rest - V6 directory

--- FS1Rest - V6/DALs/membersMongo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-let membersSchema = new mongoose.Schema({
-    Name : String,
-    Email : String,
-    City : String
-})
-
-module.exports = mongoose.model('members', membersSchema)
--- FS1Rest - V6/DALs/moviesWS.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios')
-
-const getMoviesWS = () => {
-    return axios.get("https://api.tvmaze.com/shows")
-}
-
-module.exports = {getMoviesWS}

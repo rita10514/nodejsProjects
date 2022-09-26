b954de294c4c463ab8c4ea64df8c7d20dbdb2ee0
Pinhasov_Margarita <45937245+rita10514@users.noreply.github.com>
2022-09-26 20:38:56 +0300

    Delete CinimaDataMngSite-FinalProject/FS1WS - V12/CINEMA directory

--- CinimaDataMngSite-FinalProject/FS1WS - V12/CINEMA/DALs/usersMongo.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-let usersSchema = new mongoose.Schema({
-    userName : String,
-    psw : String
-})
-
-module.exports = mongoose.model("users", usersSchema)
